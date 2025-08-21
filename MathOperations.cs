217a173edd3e11e280883b21070997b7056305d4
Caron Haynes <caronhaynescruz@gmail.com>
2025-08-21 04:06:44 -0400

    Add Calling Methods with class and three maths methods

---
+++ MathOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    // This class has three methods that do different math operations
+    public class MathOperations
+    {
+        // Multiply the input number by 5
+        public int MultiplyByFive(int number)
+        {
+            return number * 5; // return the result
+        }
+
+        // Add 20 to the input number
+        public int AddTwenty(int number)
+        {
+            return number + 20; // return the result
+        }
+
+        // Subtract 7 from the input number
+        public int SubtractSeven(int number)
+        {
+            return number - 7; // return the result
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of the MathOperations class
+            MathOperations math = new MathOperations();
+
+            // Ask the user to type a number
+            Console.WriteLine("Hey! Enter a number to see some math magic:");
+            int userInput = Convert.ToInt32(Console.ReadLine()); // convert input to integer
+
+            // Call the MultiplyByFive method
+            int multiplied = math.MultiplyByFive(userInput);
+            Console.WriteLine("Your number multiplied by 5 is: " + multiplied);
+
+            // Call the AddTwenty method
+            int added = math.AddTwenty(userInput);
+            Console.WriteLine("Your number plus 20 is: " + added);
+
+            // Call the SubtractSeven method
+            int subtracted = math.SubtractSeven(userInput);
+            Console.WriteLine("Your number minus 7 is: " + subtracted);
+
+            // Keep the console open
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
+
+
