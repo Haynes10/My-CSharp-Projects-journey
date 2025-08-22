c4e157a9a0f9fe073108f2e21ab2053db884d54b
Caron Haynes <caronhaynescruz@gmail.com>
2025-08-22 18:40:18 -0400

    Adding Abstract Class Assignment

---
+++ Employee2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class Employee2 : Person2
+    {
+        //We are now required to give our own version of SayName()
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName); //Prints the full name to the console
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Make a new Employee object
+            //Give it some values
+            //Call the method to show the name
+            Employee2 employee2 = new Employee2();
+            employee2.FirstName = "Kobe";
+            employee2.LastName = "Bryant";
+            employee2.SayName();
+
+            Console.ReadLine(); //Make the conosle not close right away
+
+        }
+    }
+}
