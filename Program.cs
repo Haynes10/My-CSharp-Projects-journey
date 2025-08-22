76ad27606c7ddb1940c182743d94bbed517d398e
Caron Haynes <caronhaynescruz@gmail.com>
2025-08-22 04:18:10 -0400

    Console App with Person and Employee with inheritance

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate an Employee object with name
+            Employee employee = new Employee();
+            {
+                employee.FirstName = "Mike";
+                employee.LastName = "Tyson";
+                employee.Id = 1234;
+            }
+            // Call to SayName method from the Person class on the Employee object
+            //Because Employee inherits from Person, this work perfectly
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
