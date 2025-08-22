4ac92144b6e6aa104f54ee242fe248a81d8cb485
Caron Haynes <caronhaynescruz@gmail.com>
2025-08-22 02:05:14 -0400

    Add Class Method Console App

---
+++ MathMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodApp
+{
+    public class MathMethods
+    {
+        //Void method that takes an integer, divides it by 2, and prints the result
+        public static void DivideByTwo(int number)
+        {
+            int result = number / 2; // Divide number by 2
+            Console.WriteLine($"Your number divided by 2 is: {result}");
+        }
+
+        //Method with output parameters: returns sum and product of two numbers
+        public static void Calculate(int a, int b, out int sum, out int product)
+        {
+            sum = a + b;
+            product = a * b;
+        }
+
+        //Overloaded method: same name, but works with doubles instead of ints
+        public static double DivideByTwo(double number)
+        {
+            return number / 2.0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user for a number
+            Console.WriteLine("Please enter a number: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Call the static method DivideByTwo (int version)
+            MathMethods.DivideByTwo(userNumber);
+
+            //Demonstrate method  Overloading(double version)
+            double result = MathMethods.DivideByTwo(7.5);
+            Console.WriteLine($"7.5 divided by 2 is: {result}");
+
+            //Demonstrate method with output parameters
+            int sum, product;
+            MathMethods.Calculate(5, 10, out sum, out product);
+            Console.WriteLine($"Using output parameters: Sum = {sum}, Product = {product}");
+
+            Console.ReadLine();
+        }
+    }
+}
