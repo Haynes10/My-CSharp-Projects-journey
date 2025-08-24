77e8770b41626c90f1953b8b795eb388756ce2dc
Caron Haynes <caronhaynescruz@gmail.com>
2025-08-23 20:15:58 -0400

    Add Struct Assignment Code

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+
+    namespace StructAssignment
+    {
+        class Program
+        {
+            // Define a struct named Number
+            // A struct is like a class but it's a value type instead of a reference type
+            public struct Number
+            {
+                // Property "Amount" of type decimal (good for money or precise numbers)
+                public decimal Amount { get; set; }
+            }
+
+            static void Main(string[] args)
+            {
+                // Create a new object of type Number
+                Number myNumber = new Number();
+
+                // Assign a value to the Amount property
+                // I'm just using a random example amount, but this could be any decimal number
+                myNumber.Amount = 432.10m;
+
+                // Print the value of Amount to the console
+                Console.WriteLine($"The amount stored in myNumber is: {myNumber.Amount}");
+
+                // Keeps the console open so I can see the output before it closes
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
