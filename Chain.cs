3dd972eacb00e7817c042104521df58347801117
Caron Haynes <caronhaynescruz@gmail.com>
2025-08-25 18:37:00 -0400

    Added console app with const, var, constructor chaining

---
+++ Chain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class Chain
+    {
+        // Auto-properties for Name and Num
+        public string Name { get; set; }
+        public float Num { get; set; }
+
+        // Constructor if only a number is provided
+        public Chain(float num) : this(num, "Rolly")
+        {
+            // This runs after calling the two-parameter constructor
+            Console.WriteLine($"{Num} and your default name is {Name}");
+        }
+
+        // Constructor if only a name is provided
+        public Chain(string name) : this(42, name)
+        {
+            // This runs after calling the two-parameter constructor
+            Console.WriteLine($"{Name} and your default favorite number is {Num}");
+        }
+
+        // Constructor if both a number and name are provided
+        public Chain(float num, string name)
+        {
+            Name = name;
+            Num = num;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{ 
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            // Create const variables (unchangeable)
+            const string const1 = "Your name is: ";
+            const string const2 = "Your favorite number is: ";
+
+            // Prompt user for input
+            Console.WriteLine("Enter your name or favorite number: ");
+
+            // Create a variable using 'var' keyword (type inferred from input)
+            var answer = Console.ReadLine();
+
+            // Try parsing the answer as a number
+            if (float.TryParse(answer, out float x))
+            {
+                // If it's a number, use the numeric constructor
+                Console.WriteLine(const2);
+                Chain newChain = new Chain(x);
+                Console.ReadLine();
+            }
+            else
+            {
+                // If it's not a number, treat it as a name
+                Console.WriteLine(const1);
+                Chain newChain = new Chain(answer);
+                Console.ReadLine();
+            }
+        }
+    }
+}
