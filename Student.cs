2504de48bafdae187cab972e5448e89a35afa008
Caron Haynes <caronhaynescruz@gmail.com>
2025-08-29 08:52:34 -0400

    Adding Student App Project

---
+++ Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentApp
+{
+   public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
